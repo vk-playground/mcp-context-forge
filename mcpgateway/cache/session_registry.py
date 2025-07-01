05266f13154c67a76445de56b6d1bd68c4c0f10b
Madhav Kandukuri <madhav165@gmail.com>
2025-07-01 12:30:33 +0530

    Fix test_redis_error_handling Signed-off-by: Madhav Kandukuri <madhav165@gmail.com>

--- mcpgateway/cache/session_registry.py
+++ mcpgateway/cache/session_registry.py
@@ -94,7 +94,6 @@ class SessionBackend:
                 raise ValueError("Redis backend requires redis_url")
 
             self._redis = Redis.from_url(redis_url)
-            self._pubsub = self._redis.pubsub()
 
         elif self._backend == "database":
             if not SQLALCHEMY_AVAILABLE:
@@ -152,6 +151,7 @@ class SessionRegistry(SessionBackend):
             logger.info("Database cleanup task started")
 
         elif self._backend == "redis":
+            self._pubsub = self._redis.pubsub()
             await self._pubsub.subscribe("mcp_session_events")
 
         elif self._backend == "none":
