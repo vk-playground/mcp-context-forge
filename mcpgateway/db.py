c343306597e22c673742c2b635d572b02bfd44c1
Reeve Barreto <81756283+reevebarreto@users.noreply.github.com>
2025-07-03 12:24:20 +0100

    fix: added db retry mechanism to mcpgateway/db.py (#226)
    * fix: added db retry mechanism to mcpgateway/db.py
    
    Signed-off-by: reevebarreto <reevemarcbarreto@gmail.com>
    
    * cleaned code to a one liner
    
    Signed-off-by: reevebarreto <reevemarcbarreto@gmail.com>
    
    * Flake8 fix
    
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>
    
    ---------
    
    Signed-off-by: reevebarreto <reevemarcbarreto@gmail.com>
    Signed-off-by: Mihai Criveti <crivetimihai@gmail.com>
    Co-authored-by: Mihai Criveti <crivetimihai@gmail.com>

--- mcpgateway/db.py
+++ mcpgateway/db.py
@@ -25,6 +25,7 @@ import uuid
 from mcpgateway.config import settings
 from mcpgateway.types import ResourceContent
 from mcpgateway.utils.create_slug import slugify
+from mcpgateway.utils.db_isready import wait_for_db_ready
 
 # Third-Party
 import jsonschema
@@ -1220,4 +1221,7 @@ def init_db():
 
 
 if __name__ == "__main__":
+    # Wait for database to be ready before initializing
+    wait_for_db_ready(max_tries=int(settings.db_max_retries), interval=int(settings.db_retry_interval_ms) / 1000, sync=True)  # Converting ms to s
+
     init_db()
